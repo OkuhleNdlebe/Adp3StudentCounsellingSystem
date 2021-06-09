ef43830aa3a057e33cd6ba73f358c6f6f0094597
Lindokuhle Nini <lindokuhlepaul6@gmail.com>
2021-06-09 22:16:09 +0200

    first commit

---
+++ src/main/java/za/ac/cput/entity/generic/Booking.java
@@ -0,0 +1,9 @@
+package za.ac.cput.entity.generic;
+
+/**Booking.java
+ * Entity for Booking a counselling session
+ * Author: Lindokuhle Nini (218196504)
+ * Date: 9 June 2021
+ */
+public class Booking {
+}
---
+++ src/main/java/za/ac/cput/entity/generic/BookingStatus.java
@@ -0,0 +1,8 @@
+package za.ac.cput.entity.generic;
+/**BookingStatus.java
+ * Entity for the Booking Status
+ * Author: Lindokuhle Nini (218196504)
+ * Date: 9 June 2021
+ */
+public class BookingStatus {
+}
