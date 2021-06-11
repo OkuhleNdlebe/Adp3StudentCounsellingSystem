a061c813e79e5f4684a0f02a7cc64702dace10ee
Lindokuhle Nini <lindokuhlepaul6@gmail.com>
2021-06-12 00:22:22 +0200

    final commit

--- src/test/java/za/ac/cput/factory/BookingHistoryFactoryTest.java
+++ src/test/java/za/ac/cput/factory/BookingHistoryFactoryTest.java
@@ -1,29 +1,28 @@
 
-/*ContactDetailFactory.java
- Test case for Contact detail
+/*BookingHistory.java
+ Test Case for Booking History
  Author: Lihle Njobe-(218193882)
  Date: 9 June 2021
  */
 
-package za.ac.cput;
-
+package za.ac.cput.factory;
 import org.junit.Test;
 import org.junit.jupiter.api.Disabled;
+import za.ac.cput.entity.generic.BookingHistory;
 import za.ac.cput.entity.generic.ContactDetail;
-import za.ac.cput.factory.generic.ContactDetailFactory;
+import za.ac.cput.factory.generic.BookingHistoryFactory;
+
 
 import static org.junit.Assert.*;
 
-public class ContactDetailFactoryTest {
+public class BookingHistoryFactoryTest {
 
 
     @Test
-    public void createContactDetail() {
-        ContactDetail contactDetail= ContactDetailFactory.createContactDetail("Lihlenjibe@gmail.com", 9098908);
-        System.out.print(contactDetail);
-
+    public void createBookingHistory() {
+        BookingHistory bookingHistory= BookingHistoryFactory.createBookingHistory(67, 90, "Satisfied");
+        System.out.print(bookingHistory);
     }
-
     @Test
     public void testEquality() {
         ContactDetail.Builder contact2 = new ContactDetail.Builder().setEmailAddress("lihlenjobe@gmail.com").setPhoneNumber(0);
@@ -32,30 +31,33 @@ public class ContactDetailFactoryTest {
         assertEquals(contact2, contact1);
         System.out.print("Equal");
 
-    }
 
+    }
     @Test
-    public void testIdentity() {
+    public void testIdentity(){
         ContactDetail.Builder contact2 = new ContactDetail.Builder().setEmailAddress("lihlenjobe@gmail.com").setPhoneNumber(0);
         ContactDetail.Builder contact1 = new ContactDetail.Builder().setEmailAddress("lihlenjobe@gmail.com").setPhoneNumber(0);
         contact2 = contact1;
         assertSame(contact2, contact1);
-        System.out.print("The same");
+        System.out.print("Same");
+
     }
 
+    @Test
+    public void testTimeouts() {
 
-   @Test
+    }
+    @Test
     public void testTimeout(){
         AssertTimeout(5);
-   }
+    }
 
     private void AssertTimeout(int i) {
     }
 
     @Test
     @Disabled(" When the timeout is exceeded")
-        public void testWillBeExecuted() {
-
+    public void testWillBeExecuted() {
 
     }
 
