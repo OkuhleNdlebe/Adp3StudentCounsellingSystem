76b5351eaf2455e34cd152fcc5a656b6894a7c2b
Lindokuhle Nini <lindokuhlepaul6@gmail.com>
2021-06-11 19:29:06 +0200

    BookingFactory completed

--- src/main/java/za/ac/cput/Util/GenericHelper.java
+++ src/main/java/za/ac/cput/Util/GenericHelper.java
@@ -3,7 +3,7 @@ package za.ac.cput.Util;
 import java.util.UUID;
 
 public class GenericHelper {
-    public static String GenericHelper(){
+    public static String generateId(){
         return UUID.randomUUID().toString();
     }
 }
