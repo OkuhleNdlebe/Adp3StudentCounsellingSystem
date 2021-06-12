ae480b7b8a59c4549de6f3db6ee582b2de1c8203
vuyisa01 <218192215@mycput.ac.za>
2021-06-12 02:58:44 +0200

    Final Commit

--- src/test/java/RoleFactoryTest.java
+++ src/test/java/RoleFactoryTest.java
@@ -4,18 +4,57 @@
     Date: 11 June 2021
  */
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.entity.generic.Role;
-import za.ac.cput.factory.generic.RoleFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 class RoleFactoryTest {
 
     @Test
-    public void createRole(){
-        Role role = RoleFactory.createRole(2181922,177,"Counselling", "Counsellor");
-        System.out.println(role);
+    public void RoleFactoryTest(){
+
+        Role r1 = new Role.Builder().build();
+        Role r2 = new Role.Builder().setUserId(2181922)
+                .build();
+        Role r3 = new Role.Builder().setUserId(2181923)
+                .build();
+        Role r4 = r1;
+
+        assertNotSame(r2, r3);
+        System.out.println("r2 NOT IDENTICAL to r3");
+
+        assertSame(r1, r4);
+        System.out.println("T1 IS IDENTICAL to T4");
     }
 
+    @org.junit.Test
+    public void equalityTest(){
+
+        Role r1 = new Role.Builder().build();
+        Role r2 = new Role.Builder().setUserId(2181922)
+                .build();
+        Role r3 = new Role.Builder().setUserId(2181923)
+                .build();
+        Role r4 = r1;
+
+        assertNotEquals(r2, r3);
+        System.out.println("r2 NOT EQUAL to r3");
+
+        assertEquals(r1, r4);
+        System.out.println("r1 IS EQUAL to r4");
+
+    }
+
+    @org.junit.Test(timeout = 500)
+    public void timeoutTest(){
+        System.out.println("timeout test");
+    }
+
+    @Ignore
+    @org.junit.Test
+    public void disablingTest(){
+        System.out.println("test ignored");
+    }
 }
--- src/test/java/UserRoleFactoryTest.java
+++ src/test/java/UserRoleFactoryTest.java
@@ -4,15 +4,57 @@
     Date: 11 June 2021
      */
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.entity.generic.UserRole;
-import za.ac.cput.factory.generic.UserRoleFactory;
+
+import static org.junit.Assert.*;
 
 class UserRoleFactoryTest {
     @Test
-    public void createUserRole(){
-        UserRole userrole = UserRoleFactory.createUserRole(2181922, 125);
-        System.out.println(userrole);
+    public void UserRoleFactoryTest(){
+
+        UserRole ur1 = new UserRole.Builder().build();
+        UserRole ur2 = new UserRole.Builder().setUserId(2181922)
+                .build();
+        UserRole ur3 = new UserRole.Builder().setUserId(2181923)
+                .build();
+        UserRole ur4 = ur1;
+
+        assertNotSame(ur2, ur3);
+        System.out.println("r2 NOT IDENTICAL to r3");
+
+        assertSame(ur1, ur4);
+        System.out.println("T1 IS IDENTICAL to T4");
+    }
+
+    @org.junit.Test
+    public void equalityTest(){
+
+        UserRole ur1 = new UserRole.Builder().build();
+        UserRole ur2 = new UserRole.Builder().setUserId(2181922)
+                .build();
+        UserRole ur3 = new UserRole.Builder().setUserId(2181923)
+                .build();
+        UserRole ur4 = ur1;
+
+        assertNotEquals(ur2, ur3);
+        System.out.println("r2 NOT EQUAL to r3");
+
+        assertEquals(ur1, ur4);
+        System.out.println("r1 IS EQUAL to r4");
+
+    }
+
+    @org.junit.Test(timeout = 500)
+    public void timeoutTest(){
+        System.out.println("timeout test");
+    }
+
+    @Ignore
+    @org.junit.Test
+    public void disablingTest(){
+        System.out.println("test ignored");
     }
 
 }
