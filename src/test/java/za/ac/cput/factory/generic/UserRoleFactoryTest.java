874345612ff701aa8387d43dbdbfa7d96c8c01cd
vuyisa01 <218192215@mycput.ac.za>
2021-06-12 10:15:21 +0200

    Final Commit

--- src/test/java/za/ac/cput/factory/generic/UserRoleFactoryTest.java
+++ src/test/java/za/ac/cput/factory/generic/UserRoleFactoryTest.java
@@ -1,60 +1,60 @@
-/* RoleFactoryTest.java
-    Testing for Role Factory
-    Author: Vuyisa Nkangana(218192215)
-    Date: 11 June 2021
- */
+package za.ac.cput.factory.generic;
 
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
-import za.ac.cput.entity.generic.Role;
+import za.ac.cput.entity.generic.UserRole;
 
 import static org.junit.Assert.*;
 
-class RoleFactoryTest {
+public class UserRoleFactoryTest {
 
     @Test
-    public void RoleFactoryTest(){
+    public void UserRoleFactoryTest(){
 
-        Role r1 = new Role.Builder().build();
-        Role r2 = new Role.Builder().setUserId(2181922)
+        UserRole ur1 = new UserRole.Builder().build();
+        UserRole ur2 = new UserRole.Builder().setUserId(2181922)
                 .build();
-        Role r3 = new Role.Builder().setUserId(2181923)
+        UserRole ur3 = new UserRole.Builder().setUserId(2181923)
                 .build();
-        Role r4 = r1;
+        UserRole ur4 = ur1;
 
-        assertNotSame(r2, r3);
+        assertNotSame(ur2, ur3);
         System.out.println("r2 NOT IDENTICAL to r3");
 
-        assertSame(r1, r4);
+        assertSame(ur1, ur4);
         System.out.println("T1 IS IDENTICAL to T4");
     }
 
     @org.junit.Test
     public void equalityTest(){
 
-        Role r1 = new Role.Builder().build();
-        Role r2 = new Role.Builder().setUserId(2181922)
+        UserRole ur1 = new UserRole.Builder().build();
+        UserRole ur2 = new UserRole.Builder().setUserId(2181922)
                 .build();
-        Role r3 = new Role.Builder().setUserId(2181923)
+        UserRole ur3 = new UserRole.Builder().setUserId(2181923)
                 .build();
-        Role r4 = r1;
+        UserRole ur4 = ur1;
 
-        assertNotEquals(r2, r3);
+        assertNotEquals(ur2, ur3);
         System.out.println("r2 NOT EQUAL to r3");
 
-        assertEquals(r1, r4);
+        assertEquals(ur1, ur4);
         System.out.println("r1 IS EQUAL to r4");
 
     }
 
     @org.junit.Test(timeout = 500)
     public void timeoutTest(){
+
         System.out.println("timeout test");
     }
 
     @Ignore
     @org.junit.Test
     public void disablingTest(){
+
         System.out.println("test ignored");
     }
+
+
 }
